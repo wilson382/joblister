353884b03e1cfc28d0acc355a58b036a907e965f
Wilson - Labtop <rbenavides382@gmail.com>
2020-02-02 09:29:16 -0500

    Discontinued __autoloader(),now using spl_autoloader_register()
    Signed-off-by: Wilson - Labtop <rbenavides382@gmail.com>

--- config/init.php
+++ config/init.php
@@ -8,8 +8,16 @@ require_once(__DIR__ ."/config.php");
 //Config file
 require_once(__DIR__ . "/../helpers/system_helper.php");
 
-#Class Auto Loader
-function __autoload($class_name){
-	require_once(__DIR__ . "/../libs/$class_name.php");
+#Class Auto Loader function
+function my_autoloader($class_name){
+	$class_path=__DIR__ . "/../classes/$class_name.php";
+
+	if (!is_file($class_path)){
+		return false;
+	}
+
+	require_once($class_path);
 }
+
+spl_autoload_register("my_autoloader");
 ?>
