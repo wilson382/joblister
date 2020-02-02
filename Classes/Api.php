0de7878a7f2f99ca1063f5ebf00482d421493f10
Wilson - Labtop <rbenavides382@gmail.com>
2020-02-02 09:28:32 -0500

    Removed usage of token_name as part of the api auth.
    Id and password is sufficient enough
    Signed-off-by: Wilson - Labtop <rbenavides382@gmail.com>

--- Classes/Api.php
+++ Classes/Api.php
@@ -7,13 +7,10 @@ class Api{
 		$this->job= new Job();
 	}
 
-	public function validateToken($token_name,$token_id,$token_password){
-		$db= new Database;
+	public function validateToken(string $token_id, string $token_password){
+		$db= new Database();
 
-		$db->query("SELECT * FROM `api_tokens`WHERE `token_name`=:token_name
-		AND `token_id`=:token_id AND `token_password`=:token_password;");
-
-		$db->bind(":token_name",$token_name);
+		$db->query("SELECT * FROM `api_tokens`WHERE `token_id`=:token_id AND `token_password`=:token_password;");
 		$db->bind(":token_id",$token_id);
 		$db->bind(":token_password",$token_password);
 		$row=$db->single();
@@ -31,12 +28,11 @@ class Api{
 	}
 }
 
-if (isset($_GET['name']) && isset($_GET['id']) && isset($_GET['pw'])){
-	$token_name=$_GET['name'];
+if (isset($_GET['id']) && isset($_GET['pw'])){
 	$token_id=$_GET['id'];
 	$token_password=$_GET['pw'];
 	$api = new Api;
-	if ($api->validateToken($token_name,$token_id,$token_password)){
+	if ($api->validateToken($token_id,$token_password)){
 		header("Content-Type: application/json");
 		echo $api->getAllJobs();
 	}else{
